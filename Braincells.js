966774a3f4a6308a74b2d8bd3fd43f6817ce9e63
AR789-2 <abiodun.ramon@mnstu.catholic.edu.au>
2024-10-18 11:06:44 +1100

    Add files via upload

---
+++ Braincells.js
@@ -0,0 +1,22 @@
+const MARKER = Array(10, 1)
+//Array(MONTH, DAY)
+
+let IIASD = false;
+const day = new Date()
+
+if (day.getMonth() == MARKER[0]) {
+    if (day.getDate() == MARKER[1]) {
+        IIASD = true
+    }
+}
+
+if (IIASD) {
+    IIASD = 'YES'
+    document.getElementById("x").style.color = 'Green';
+}
+else {
+    IIASD = 'NO'
+    document.getElementById("x").style.color = 'Red';
+}
+
+document.getElementById("x").innerHTML = IIASD
